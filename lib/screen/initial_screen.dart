b9177f1d9f1ea9b8b57aa190ccfac02f3111286f
Jackson Ribeiro <jacksonribeiro@bemol.com.br>
2024-11-27 12:22:42 -0400

    add switch case e validação de dados.

--- lib/screen/initial_screen.dart
+++ lib/screen/initial_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/components/task.dart';
+import 'package:task_app/data/task_dao.dart';
 import 'package:task_app/data/task_inherited.dart';
 import 'package:task_app/screen/task_form.dart';
 
@@ -22,9 +24,44 @@ class _InitialScreenState extends State<InitialScreen> {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: ListView(
-        padding: const EdgeInsets.only(top: 10, bottom: 70),
-        children: TaskInherited.of(context).taskList,
+      body: Padding(padding: const EdgeInsets.only(top: 10, bottom: 70),
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(), builder: (context, snapshot) {
+          List<Task>? items = snapshot.data;
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            // TODO: Handle this case.
+            case ConnectionState.waiting:
+            // TODO: Handle this case.
+            case ConnectionState.waiting:
+            // TODO: Handle this case.
+            case ConnectionState.active:
+            // TODO: Handle this case.
+            case ConnectionState.done:
+              if (snapshot.hasData && items != null) {
+                if (items.isNotEmpty) {
+                  return ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final Task tasks = items[index];
+                        return tasks;
+                      });
+                }
+                return Center(
+                  child: Column(
+                    children: [
+                      Icon(Icons.error_outline, size: 128,),
+                      Text("Não há nenhuma tarefa",
+                        style: TextStyle(fontSize: 32,),)
+                    ],
+                  ),
+                );
+              }
+              return Text("Erro ao carregar tarefas.");
+              break;
+          }
+          return Text("Erro desconhecido");
+        }),
       ),
       backgroundColor: const Color(0xFFD3F1F8),
       floatingActionButton: FloatingActionButton(
@@ -33,8 +70,9 @@ class _InitialScreenState extends State<InitialScreen> {
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (contextNew) => FormScreen(
-                    taskContext: context,
+                  builder: (contextNew) =>
+                      FormScreen(
+                        taskContext: context,
                       )));
         },
         child: const Icon(
